6c04e9c3a0978a4de2c5525be4ecdd7d859e4729
Darya <darya.ogurtsova.99@mail.ru>
2022-09-18 23:59:06 +0300

    Добавлено решение задания

---
+++ Ex/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число:");
+     string n1 = Console.ReadLine();
+     int n = Convert.ToInt32(n1);
+     int i;
+     
+     bool otv = true;
+     for (i=2;i<n;i++) {
+        if (n % i == 0) {
+            otv = false;
+        }
+     }
+     if (otv == true){
+        Console.WriteLine("число простое");
+     } else{
+        Console.WriteLine("число не простое");
+     }
+    
+
+    
